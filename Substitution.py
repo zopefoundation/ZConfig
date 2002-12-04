d26ded89376da176adbfa4e2abccdd4593a16b67
Fred Drake <fdrake@acm.org>
2002-12-04 22:48:31 +0000

    Clean out a bunch of the ZConfig.Substitution module that is no longer useful now that %define and substitution is built into the basic handling of the configuration files.

--- Substitution.py
+++ Substitution.py
@@ -5,67 +5,23 @@ try:
 except NameError:
     False = 0
 
-class SubstitutionError(Exception):
-    """Base exception for string substitution errors."""
+class SubstitutionSyntaxError(Exception):
+    """Raised when interpolation source text contains syntactical errors."""
 
-    def __init__(self, msg, context):
+    def __init__(self, msg):
         self.message = msg
-        if context is not None:
-            context = context[:]
-        self.context = context
 
     def __str__(self):
         return self.message
 
-class SubstitutionSyntaxError(SubstitutionError):
-    """Raised when interpolation source text contains syntactical errors."""
 
-    def __init__(self, msg, context):
-        SubstitutionError.__init__(self, msg, context)
-
-class SubstitutionRecursionError(SubstitutionError):
-    """Raised when a nested interpolation is recursive."""
-
-    def __init__(self, name, context):
-        self.name = name
-        msg = ("recursion on %s; current context:\n%s"
-               % (repr(name), ", ".join(context)))
-        SubstitutionError.__init__(self, msg, context)
-
-
-def get(section, name, default=None):
-    # XXX should this interpolate from default if that's what's used?
-    missing = []
-    s = section.get(name, missing)
-    if s is missing:
-        return default
-    if "$" in s:
-        accum = []
-        _interp(accum, s, section, [name])
-        s = ''.join(accum)
-    return s
-
-
-def substitute(s, section):
+def substitute(s, mapping):
     """Interpolate variables from `section` into `s`."""
     if "$" in s:
         accum = []
-        _interp(accum, s, section, None)
+        _interp(accum, s, mapping)
         s = ''.join(accum)
     return s
-
-
-def getnames(s):
-    """Return a list of names referenced by s."""
-    if "$" in s:
-        L = []
-        while s:
-            p, name, s = _split(s, None)
-            if name and name not in L:
-                L.append(name)
-        return L
-    else:
-        return []
 
 
 def isname(s):
@@ -77,33 +33,19 @@ def isname(s):
         return False
 
 
-def _interp(accum, rest, section, context):
+def _interp(accum, rest, mapping):
     while 1:
-        s, name, rest = _split(rest, context)
+        s, name, rest = _split(rest)
         if s:
             accum.append(s)
         if name:
-            v = section.get(name)
-            if v is None:
-                parent = getattr(section, "container", None)
-                while parent is not None:
-                    v = parent.get(name)
-                    if v is not None:
-                        break
-                    parent = getattr(parent, "container", None)
-                else:
-                    v = ""
-            if "$" in v and context:
-                if name in context:
-                    raise SubstitutionRecursionError(name, context)
-                _interp(accum, v, section, context + [name])
-            else:
-                accum.append(v)
+            v = mapping.get(name, "")
+            accum.append(v)
         if not rest:
             return
 
 
-def _split(s, context):
+def _split(s):
     # Return a triple:  prefix, name, suffix
     # - prefix is text that can be used literally in the result (may be '')
     # - name is a referenced name, or None
@@ -120,13 +62,12 @@ def _split(s, context):
         if c == "{":
             m = _name_match(s, i + 2)
             if not m:
-                raise SubstitutionSyntaxError("'${' not followed by name",
-                                              context)
+                raise SubstitutionSyntaxError("'${' not followed by name")
             name = m.group(0)
             i = m.end() + 1
             if not s.startswith("}", i - 1):
                 raise SubstitutionSyntaxError(
-                    "'${%s' not followed by '}'" % name, context)
+                    "'${%s' not followed by '}'" % name)
         else:
             m = _name_match(s, i+1)
             if not m:
--- tests/testSubstitution.py
+++ tests/testSubstitution.py
@@ -5,18 +5,7 @@ from __future__ import nested_scopes
 
 import unittest
 
-from types import StringType
-from UserDict import UserDict
-
-from ZConfig.Substitution import get, getnames, isname, substitute
-from ZConfig.Substitution import SubstitutionRecursionError
-from ZConfig.Substitution import SubstitutionSyntaxError
-
-
-class ContainerDict(UserDict):
-    def __init__(self, mapping=None, container=None):
-        self.container = container
-        UserDict.__init__(self, mapping)
+from ZConfig.Substitution import isname, substitute, SubstitutionSyntaxError
 
 
 class SubstitutionTestCase(unittest.TestCase):
@@ -52,15 +41,12 @@ class SubstitutionTestCase(unittest.TestCase):
     def test_syntax_errors(self):
         d = {"name": "${next"}
         def check(s):
-            self.check_error_context(SubstitutionSyntaxError, None,
-                                     substitute, s, d)
+            self.assertRaises(SubstitutionSyntaxError,
+                              substitute, s, d)
         check("${")
         check("${name")
         check("${1name}")
         check("${ name}")
-
-        self.check_error_context(SubstitutionSyntaxError, ["name"],
-                                 get, d, "name")
 
     def test_edge_cases(self):
         # It's debatable what should happen for these cases, so we'll
@@ -74,66 +60,6 @@ class SubstitutionTestCase(unittest.TestCase):
     def test_non_nesting(self):
         d = {"name": "$value"}
         self.assertEqual(substitute("$name", d), "$value")
-
-    def test_simple_nesting(self):
-        d = {"name": "value",
-             "nest": "$splat",
-             "splat": "nested"}
-        def check(name, value):
-            self.assertEqual(get(d, name), value)
-        check("name", "value")
-        check("nest", "nested")
-
-    def test_nesting_errors(self):
-        d = {"name": "$splat",
-             "splat": "$name"}
-        self.check_error_context(SubstitutionRecursionError, ["name", "splat"],
-                                 get, d, "name")
-        d = {"name": "$splat",
-             "splat": "$splat"}
-        self.check_error_context(SubstitutionRecursionError, ["name", "splat"],
-                                 get, d, "name")
-
-    def test_container_search(self):
-        d1 = {"outer": "outervalue",
-              "inner": "inner-from-outer"}
-        d2 = ContainerDict({"inner": "inner-from-inner",
-                            "bogus": "${nothere}",
-                            "both": "${inner} ${outer}"}, d1)
-        self.assertEqual(get(d2, "both"),
-                         "inner-from-inner outervalue")
-        self.assertEqual(get(d2, "bogus"), "")
-
-    def check_error_context(self, exc, context, callable, *args, **kw):
-        try:
-            callable(*args, **kw)
-        except exc, e:
-            self.assertEqual(e.context, context)
-        else:
-            if isinstance(exc, StringType):
-                name = `exc`
-            elif exc.module == "exceptions":
-                # Built-in exceptions
-                name = exc.__name__
-            else:
-                name = exc.__module__ + "." + exc.__name__
-            self.fail("expected exception " + name)
-
-    def test_getnames(self):
-        self.assertEqual(getnames(""), [])
-        self.assertEqual(getnames("abc"), [])
-        self.assertEqual(getnames("$"), [])
-        self.assertEqual(getnames("$$"), [])
-        self.assertEqual(getnames("$abc"), ["abc"])
-        self.assertEqual(getnames("$abc$abc"), ["abc"])
-        self.assertEqual(getnames("$abc$def"), ["abc", "def"])
-        self.assertEqual(getnames("${abc}${def}"), ["abc", "def"])
-        self.assertEqual(getnames("$abc xyz${def}pqr$def"), ["abc", "def"])
-        self.assertEqual(getnames("$ABC xyz${def}pqr$DEF"), ["abc", "def"])
-        self.assertRaises(SubstitutionSyntaxError, getnames, "${")
-        self.assertRaises(SubstitutionSyntaxError, getnames, "${name")
-        self.assertRaises(SubstitutionSyntaxError, getnames, "${1name}")
-        self.assertRaises(SubstitutionSyntaxError, getnames, "${ name}")
 
     def test_isname(self):
         self.assert_(isname("abc"))
