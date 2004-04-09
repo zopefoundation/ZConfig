354726fb483ac4849d9c9aacf0e33c0bf842341f
Fred Drake <fdrake@acm.org>
2004-04-09 21:21:59 +0000

    Change the mapping from zLOG to the logging package: - don't dump everything into the "event" logger;   use the subsystem argument to zLOG.LOG to get a specific logger - move some comments into docstrings

--- components/logger/logger.py
+++ components/logger/logger.py
@@ -17,6 +17,15 @@ from ZConfig.components.logger.factory import Factory
 
 
 class LoggerFactoryBase(Factory):
+    """Base class for logger factories.
+
+    Factory used to create loggers while delaying actual logger
+    instance construction.  We need to do this because we may want to
+    reference a logger before actually instantiating it (for example,
+    to allow the app time to set an effective user).  An instance of
+    this wrapper is a callable which, when called, returns a logger
+    object.
+    """
 
     def __init__(self, section):
         Factory.__init__(self)
@@ -44,24 +53,13 @@ class LoggerFactoryBase(Factory):
 
 
 class EventLogFactory(LoggerFactoryBase):
-    """
-    A wrapper used to create loggers while delaying actual logger
-    instance construction.  We need to do this because we may
-    want to reference a logger before actually instantiating it (for example,
-    to allow the app time to set an effective user).
-    An instance of this wrapper is a callable which, when called, returns a
-    logger object.
-    """
+    """Logger factory that returns the root logger."""
 
-    name = "event"
-
-    def create(self):
-        logger = LoggerFactoryBase.create(self)
-        logger.propagate = 0
-        return logger
+    name = None
 
 
 class LoggerFactory(LoggerFactoryBase):
+    """Logger factory that returns the named logger."""
 
     def __init__(self, section):
         LoggerFactoryBase.__init__(self, section)
