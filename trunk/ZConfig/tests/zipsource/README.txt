9074b9752571b5a7e9dd23769c62c1fa78f9e227
Fred Drake <fdrake@acm.org>
2007-06-20 22:00:44 +0000

    remove accidental cruft

--- trunk/ZConfig/tests/zipsource/README.txt
+++
@@ -1,2 +0,0 @@
-This directory contains a sample package that is used to create the
-'foosample.zip' file used in the tests.
--- trunk/ZConfig/tests/zipsource/foo/sample/datatypes.py
+++
@@ -1,7 +0,0 @@
-"""Sample datatypes used for testing.
-"""
-__docformat__ = "reStructuredText"
-
-
-def data(value):
-    return "| %s |" % value
